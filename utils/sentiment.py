c5c95e14e7268fca308e64a08063063cbb2876cd
dabila <mboya4194@gmail.com>
2025-07-14 19:33:11 +0300

    Update sentiment.py

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,6 +1,8 @@
 import numpy as np
+import random
 
 def generate_mock_sentiment(df):
     np.random.seed(42)
-    df['Sentiment'] = np.random.uniform(-1, 1, size=len(df))
+    df = df.copy()
+    df['Sentiment'] = [random.uniform(-1, 1) for _ in range(len(df))]
     return df
