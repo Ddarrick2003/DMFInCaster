d0101245498b8a2d7a3f26c13da80906ccb2195a
dabila <mboya4194@gmail.com>
2025-07-14 19:34:56 +0300

    Update garch_model.py

--- model/garch_model.py
+++ model/garch_model.py
@@ -1,11 +1,11 @@
 from arch import arch_model
-import pandas as pd
 
 def forecast_garch_var(df):
-    returns = df['Returns'].dropna() * 100
-    model = arch_model(returns, vol='GARCH', p=1, q=1)
-    res = model.fit(disp='off')
-    forecast = res.forecast(horizon=5)
-    vol = forecast.variance.values[-1]
-    var_1d = 1.65 * (vol[0] ** 0.5)
-    return pd.Series(vol), var_1d
+    returns = df['Returns'] * 100  # scale
+    model = arch_model(returns, vol='Garch', p=1, q=1)
+    fitted = model.fit(disp='off')
+    forecast = fitted.forecast(horizon=1)
+    vol = forecast.variance.values[-1, 0] ** 0.5
+    var_1d = -1.65 * vol
+    return forecast.variance[-30:], var_1d
+
