4566819df52afa7305a243f8967c9c6aef55a77a
dabila <mboya4194@gmail.com>
2025-07-20 21:07:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from sklearn.metrics import mean_absolute_error
-from model.lstm_model import run_lstm_forecast
+from models.lstm_model import run_lstm_forecast
 from models.garch_model import run_garch_forecast
 from models.xgboost_model import run_xgboost_forecast
 from transformer_models import run_transformer_model
