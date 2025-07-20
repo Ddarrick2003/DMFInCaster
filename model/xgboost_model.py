d40a04c63112378a62dcf5787d874325e2522abe
dabila <mboya4194@gmail.com>
2025-07-20 21:54:32 +0300

    Update xgboost_model.py

--- model/xgboost_model.py
+++ model/xgboost_model.py
@@ -20,19 +20,21 @@ def run_xgboost_forecast(df, forecast_days=10):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
 
-    model = XGBRegressor(n_estimators=100, learning_rate=0.1)
+    model = XGBRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
 
+    # Predict future values using the last available inputs
     last_known = df.iloc[-forecast_days:][features]
     future_preds = model.predict(last_known)
 
     mae = mean_absolute_error(y_test, preds)
 
-    shap_explainer = shap.Explainer(model)
+    shap_explainer = shap.Explainer(model, X_train)
     shap_values = shap_explainer(X_test)
 
+    # Build comparison dataframe
     actuals = y_test.reset_index(drop=True)
     predicted = pd.Series(preds, name="Predicted")
 
@@ -42,3 +44,4 @@ def run_xgboost_forecast(df, forecast_days=10):
     })
 
     return future_preds, comparison_df, mae, model, shap_values
+
